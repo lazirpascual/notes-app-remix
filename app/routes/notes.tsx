0ab310725d3110682813213ab3ddf6c38017a650
Lazir Pascual <lazirpascual@gmail.com>
2023-12-23 01:04:54 -0500

    add NoteList component and json database logic

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,12 +1,28 @@
-import type { LinksFunction } from "@remix-run/node";
+import { ActionFunctionArgs, LinksFunction, redirect } from "@remix-run/node";
 import NewNote, { links as newNoteStyles } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
+import NoteList, { links as noteListLink } from "~/components/NoteList";
 
 export default function NotesPage() {
   return (
     <main>
       <NewNote />
+      <NoteList />
     </main>
   );
 }
 
-export const links: LinksFunction = () => [...newNoteStyles()];
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toDateString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
+}
+
+export const links: LinksFunction = () => [
+  ...newNoteStyles(),
+  ...noteListLink(),
+];
